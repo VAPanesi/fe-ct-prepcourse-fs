1a2cb8725b43fe5572737200d059103f3e9c2ee1
Valentina Panesi <vapbril@outlook.com>
2023-12-22 10:54:28 -0300

    homework pch

--- M05 Fundamentos Javascript/ejercicios/10.js
+++ M05 Fundamentos Javascript/ejercicios/10.js
@@ -3,6 +3,11 @@ function esFechaValida(fecha) {
   // Comprueba si este corresponde a una fecha válida.
   // Si es así, retorna true, sino retorna false.
   // Tu código:
+  if (fecha instanceof Date && !isNaN(fecha)) {
+    return true;
+  } else {
+    return false;
+  }
 }
-
+console.log(new Date("2023-11-21"));
 module.exports = esFechaValida;
--- M05 Fundamentos Javascript/ejercicios/23.js
+++ M05 Fundamentos Javascript/ejercicios/23.js
@@ -2,6 +2,6 @@ function multiplica(x, y) {
   // La función recibe dos argumentos "x" e "y" que son números.
   // Retorna el resultado de su multiplicación.
   // Tu código:
+  return x * y ;
 }
-
 module.exports = multiplica;
